6d8273239c182ed08dacec1e26489946dbee7260
Shammi <kuldipkumar169@gmail.com>
2017-08-02 15:57:06 +0530

    last changes

---
+++ learn_webvirtue/src/main/java/test1/learn_webvirtue/App.java
@@ -0,0 +1,13 @@
+package test1.learn_webvirtue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
